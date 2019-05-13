14905adf0d8d7ac0af54aac14d00c11395a8e1ee
Rafael Aquino <rafael.acerqueirah@gmail.com>
2019-05-13 08:04:22 -0300

    Create data to social profiles

---
+++ src/data/socialProfile.js
@@ -0,0 +1,29 @@
+import emailIcon from '../assets/email_icon.png'
+import githubIcon from '../assets/github_icon'
+import linkedinIcon from '../assets/linkedin_icon'
+import twitterIcon from '../assets/twitter_icon'
+
+const SOCIAL_PROFILES = [
+  {
+    id: 1,
+    link: 'mailto:meuemail@gmail.com',
+    image: emailIcon
+  },
+  {
+    id: 2,
+    link: 'github.com',
+    image: githubIcon
+  },
+  {
+    id: 3,
+    link: 'linkedin.com',
+    image: linkedinIcon
+  },
+  {
+    id: 4,
+    link: 'twitter.com',
+    image: twitterIcon
+  }
+]
+
+export default SOCIAL_PROFILES
